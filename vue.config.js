fe93cb099644b2bf0768013439f5f969acf90685
insanprasetyo <insan.prasetyo@gmail.com>
2024-12-02 20:03:03 +0700

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  css: {
+    extract: true
+  }
+};
+
+module.exports = {   publicPath: '/crypto-ledger/', };
